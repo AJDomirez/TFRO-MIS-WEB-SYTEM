a0d8462b25599ed391de90bb405e057a73059148
@AJDomirez <domirezaj414@gmail.com>
2026-08-25 18:12:19 +0800

    Publish readable PDF form text

--- js/renewal.js
+++ js/renewal.js
@@ -3,7 +3,7 @@ import { requireRole, signOutAndRedirect } from "./auth-guard.js";
 import { logAudit } from "./audit-helper.js";
 
 async function openSavedSubmissionForm(options) {
-  const { openRenewalPdfForm } = await import("./pdf-form.js?v=20260826-011500");
+  const { openRenewalPdfForm } = await import("./pdf-form.js?v=20260826-020000");
   openRenewalPdfForm(options);
 }
 
@@ -203,7 +203,7 @@ async function showRenewalSubmission(renewal) {
 }
 
 async function showPmblCertification(renewal) {
-  const { openPmblPdfForm } = await import("./pdf-form.js?v=20260826-011500");
+  const { openPmblPdfForm } = await import("./pdf-form.js?v=20260826-020000");
   openPmblPdfForm({ renewal, franchise: currentFranchise || {} });
 }
 
