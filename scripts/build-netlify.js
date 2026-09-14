22658c71d8734e7228d2e09153fc9da823435ff5
@AJDomirez <domirezaj414@gmail.com>
2026-09-14 21:24:57 +0800

    Add search engine discovery metadata

--- scripts/build-netlify.js
+++ scripts/build-netlify.js
@@ -4,7 +4,14 @@ const path = require("node:path");
 const root = path.resolve(__dirname, "..");
 const output = path.join(root, "dist");
 const publicDirectories = ["css", "html", "js", "Logo"];
-const publicRootFiles = ["Tricycle Image.png", "index.html", ".nojekyll", "service-worker.js"];
+const publicRootFiles = [
+  "Tricycle Image.png",
+  "index.html",
+  ".nojekyll",
+  "service-worker.js",
+  "robots.txt",
+  "sitemap.xml",
+];
 const formTemplateDirectory = path.join(root, "forms");
 
 if (path.dirname(output) !== root || path.basename(output) !== "dist") {
